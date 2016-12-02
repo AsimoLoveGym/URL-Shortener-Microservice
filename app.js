4522c22be7583755044aa688948fca63451c2943
AsimoLoveGym <xiazhuangz@gmail.com>
2016-12-01 18:25:40 -0600

    [FEAT] Display stored URLs and short urls in main page, also check new url input. No short url generate for duplicate urls

--- app.js
+++ app.js
@@ -1,28 +1,28 @@
 var express = require('express');
 var app = express();
 
-var urlStringforTest = '';
-
 var handlebars = require('express-handlebars').create({ defaultLayout: null });
 app.engine('handlebars', handlebars.engine);
 app.set('view engine', 'handlebars');
 
 app.set('port', process.env.PORT || 3000);
 
+// var baseURL = + '/';
+// baseURL used for localhost development and test
 var baseURL = 'http://localhost:' + app.get('port') + '/';
 var reservedChannel = [8888, 7777, 3000];
 var urlCollection = [
   {
   original_url: 'https://www.google.com',
-  short_url: baseURL + reservedChannel[1],
+  short_url: baseURL + reservedChannel[0],
 },
 {
   original_url: 'https://www.freecodecamp.com/',
-  short_url: baseURL + reservedChannel[2],
+  short_url: baseURL + reservedChannel[1],
 },
 {
-  original_url: 'https://github.com/AsimoLoveGym',
-  short_url: baseURL + reservedChannel[3],
+  original_url: 'https://www.github.com/AsimoLoveGym',
+  short_url: baseURL + reservedChannel[2],
 },
 ];
 
@@ -36,28 +36,36 @@ app.get('/new/*', function (req, res) {
   var queryObject = req.path;
   var queryString = queryObject.slice(5);
   var outPutJson = {};
-
-  // console.log(queryString);
-  // res.send('Step in /new get handler');
+  var urlExisted = false;
 
   // Input path validation goes here
   // console.log(isURL(queryString));
   if (isURL(queryString)) {
-    var freshChannel = shortnerNumGenerator();
+    for (var i = 0; i < urlCollection.length; i++) {
+      if (urlCollection[i].original_url === queryString) {
+        outPutJson = {
+          original_url: urlCollection[i].original_url,
+          short_url: urlCollection[i].short_url,
+        };
+        urlExisted = true;
+        break;
+      }
+    }
+  }
 
+  if (isURL(queryString) && !urlExisted) {
+    var freshChannel = shortnerNumGenerator();
     var shortURL = baseURL + freshChannel;
     outPutJson = {
       original_url: queryString,
       short_url: shortURL,
     };
+
+    // update data
     urlCollection.push(outPutJson);
     reservedChannel.push(freshChannel);
   }
 
-  // console.log(queryString);
-  // console.log(shortnerNumGenerator());
-  // console.log(urlCollection);
-  // console.log(reservedChannel);
   res.json(outPutJson);
 });
 
@@ -86,9 +94,6 @@ app.use(function (req, res, next) {
 app.use(function (err, req, res, next) {
   console.error(err.stack);
   res.status(500);
-
-  // console.log(res);
-  // console.log(typeof res.statusCode);
   res.render('500');
 });
 
@@ -98,6 +103,7 @@ app.listen(app.get('port'), function () {
 
 function shortnerNumGenerator() {
   var randomChannel = 0;
+
   do {
     randomChannel = Math.floor(Math.random() * 10000);
   } while (reservedChannel.indexOf(randomChannel) > -1);
